731f6f5959e8908e79faa766b4dc0d960002c17c
0xNim <57798165+Nim1com@users.noreply.github.com>
2023-05-04 11:27:34 -0400

    merged some things together and fixed exiting

--- src/components/VersionList.js
+++ src/components/VersionList.js
@@ -95,7 +95,7 @@ const VersionList = ( modID ) => {
         </table>
         <div className="dashboard-no-mod-list-container">
             <button class="add" onClick={handleOpenVersionForm}>{isFormOpen ? 'Close' : 'Add Version'}</button>
-            {isFormOpen && <AddVersion modID={modID.modID} onCloseMenu={() => setIsFormOpen(false)}
+            {isFormOpen && <AddVersion modIDInput={modID.modID} onCloseMenu={() => setIsFormOpen(false)}
             />}
         </div>
       </div>
