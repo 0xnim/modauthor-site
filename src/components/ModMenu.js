b2287a5240aef9bdc24595896760517ab6eba2d4
0xNim <57798165+Nim1com@users.noreply.github.com>
2023-06-02 20:22:21 -0400

    updates

--- src/components/ModMenu.js
+++ src/components/ModMenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './ModMenu.css';
 import VersionList from './VersionList';
@@ -12,6 +12,7 @@ const ModMenu = ({ modObject, onCloseMenu }) => {
   const [modifiedMod, setModifiedMod] = useState(null);
   const [isVersionsOpen, setIsVersionsOpen] = useState(false);
   const [isSocialOpen, setIsSocialOpen] = useState(false);
+  const dialogRef = useRef(null);
 
   useEffect(() => {
     const apiUrl = process.env.REACT_APP_API_URL;
@@ -66,6 +67,30 @@ const ModMenu = ({ modObject, onCloseMenu }) => {
     setIsSocialOpen(!isSocialOpen);
   };
 
+  const handleDeleteMod = () => {
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const token = localStorage.getItem('accessToken');
+    axios.delete(`${apiUrl}/mods/${modId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then((response) => {
+      onCloseMenu();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  };
+
+
+  const openDialog = () => {
+    dialogRef.current.showModal();
+  };
+
+  const closeDialog = () => {
+    dialogRef.current.close();
+  };  
   
 
   if (!mod) {
@@ -108,23 +133,44 @@ const ModMenu = ({ modObject, onCloseMenu }) => {
           onChange={handleInputChange}
         />
         <div class="tag-instructions">Separate by commas</div>
+        <label for="modImage">Image:</label>
+        <input
+          type="text"
+          name="modImage"
+          value={modifiedMod.modImage}
+          readOnly={!editable}
+          onChange={handleInputChange}
+        />
       </div>
       
       {!editable ? (
-        <button onClick={() => setEditable(true)}>Edit</button>
+        <>
+          <button onClick={() => setEditable(true)}>Edit</button>
+          <button onClick={openDialog}>Delete</button>
+          <dialog ref={dialogRef} className="delete">
+            <h2>Delete</h2>
+            <p>
+              Are you sure you want to delete this version? This action cannot be undone.
+            </p>
+            <button onClick={() => handleDeleteMod(modId)} class="confirm">Confirm</button>
+            <button onClick={closeDialog}>Cancel</button>
+          </dialog>
+          <button onClick={handleSocialButton}>Social</button>
+          
+          <br></br>
+          <button onClick={handleVersionsButton}>Versions</button>
+          <br></br>
+          <button onClick={onCloseMenu}>Close</button>
+        </>
       ) : (
         <>
           <button onClick={handleCancelClick}>Cancel</button>
           <button onClick={handleSaveClick}>Save</button>
         </>
       )}
-      <button onClick={handleSocialButton}>Social</button>
       {isSocialOpen && (
       <ModInfoForm modID={modId}/>
       )}
-      {!editable && <button onClick={onCloseMenu}>Close</button> }
-      
-      {!editable && <button onClick={handleVersionsButton}>Versions</button> }
       {isVersionsOpen && (
         <VersionList modID={modId}/>
       )}
